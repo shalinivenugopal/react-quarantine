2069776dc619f8ec4c300d8ac87c8e8e43ab293c
shalinivenugopal <shalinikamini29@gmail.com>
2020-04-26 20:49:58 +0530

    something started

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+// let name = 'Kamini'
+// let obj = {
+// 	fname:'Shalini',
+// 	lname:'Venugopal'
+// }
+// const element = <h1>Welcome {name}</h1> 
+// ReactDom.render(
+// 	//element,
+// 	<h1>{obj.fname} {obj.lname}</h1>,
+// 	document.getElementById('root')
+// );
+
+
+function cartoon(name='Pikachu',show='Pokemon') {
+	//return <h1>Cartoon name is {name} and its show is {show}</h1>         //react la oru return statement la oru html tag dhan irukanum apdi illana error varumadhukaga dhan div tag use pandrom
+	return <div>
+				<h1>Cartoon name is {name} and its show is {show}</h1>
+				<h2>Hello Everyone</h2>
+			</div>
+}
+
 ReactDom.render(
-	<h1>Welcome to Bitfumes</h1>,
+	cartoon('Jasmine','Alladin'),
 	document.getElementById('root')
 );
