c9f5896b6743e9a9db0a83322bdab849558b1a0b
shalinivenugopal <shalinikamini29@gmail.com>
2020-05-05 08:17:34 +0530

    switch between 2 components

--- src/index.js
+++ src/index.js
@@ -92,7 +92,7 @@ render(){
 
 //INCREMENT APP
 
-class Inc extends React.Component{
+/*class Inc extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {counter : 0}
@@ -114,4 +114,41 @@ class Inc extends React.Component{
 ReactDom.render(
 	<Inc />,
 	document.getElementById('root')
-)
+)*/
+
+
+//SWITCH BETWEEN 2 COMPONENTS
+
+
+function Message(props){
+	if(props.value) {
+		return <h1>This is first message</h1>
+	}
+	return <h1>I am from second message</h1>
+}
+ class Btn extends React.Component{
+ 	constructor(props){
+ 		super(props);
+ 		this.state = {value:true}
+ 	}
+
+ 	handleClick = () => {
+ 		this.setState({
+ 			value : !this.state.value
+ 		});
+ 	}
+
+ 	render(){
+ 		return(
+ 			<div>
+ 				<button onClick={this.handleClick}>Change the Message</button>
+ 				<Message value={this.state.value} />
+ 			</div>
+ 		);
+ 	}
+ }
+
+ ReactDom.render(
+ 	<Btn />,
+ 	document.getElementById('root')
+ );
